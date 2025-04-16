e50ac1a214713d268cba5e759d771bf83ca457ca
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2025-04-16 13:25:13 +0200

    Change settings format to use Qt Arrays
    Before this, settings for the list of tags would be stored in a .conf
    file as QVariants that were wrappers around Python objects. These are
    unreadable, and led to issues whenever the API changed. By storing them
    in a format that QSettings expects, they become readable in the file,
    and less likely to break other parts of the code.

--- music_modify/prefs/prefs.py
+++ music_modify/prefs/prefs.py
@@ -1,6 +1,13 @@
+from dataclasses import dataclass
 from PySide6.QtCore import QSettings
-from typing import cast, final
+from typing import final
 from music_modify.custom_types.songtag import SongTag
+
+
+@dataclass(order=True, frozen=True)
+class FileTag:
+    display_name: str
+    id3_key: str
 
 
 @final
@@ -45,99 +52,118 @@ class _Settings:
     def split_values_at(self, value: str):
         self._settings.setValue("Split/split_values_at", value)
 
-    default_file_tags: list[SongTag] = [
-        SongTag(display_name="Track", id3_key="TRCK"),
-        SongTag(display_name="Title", id3_key="TIT2"),
-        SongTag(display_name="Artist", id3_key="TPE1"),
-        SongTag(display_name="Album Artist", id3_key="TPE2"),
-        SongTag(display_name="Album", id3_key="TALB"),
-        SongTag(display_name="Composer", id3_key="TCOM"),
-        SongTag(display_name="Lyricist", id3_key="TEXT"),
-        SongTag(display_name="Language", id3_key="TLAN"),
-        SongTag(display_name="BPM", id3_key="TBPM"),
-        SongTag(display_name="Key", id3_key="TKEY"),
-        SongTag(display_name="Involved People", id3_key="TIPL"),
-        SongTag(display_name="Musician Credits", id3_key="TMCL"),
+    default_file_tags: list[FileTag] = [
+        FileTag(display_name="Track", id3_key="TRCK"),
+        FileTag(display_name="Title", id3_key="TIT2"),
+        FileTag(display_name="Artist", id3_key="TPE1"),
+        FileTag(display_name="Album Artist", id3_key="TPE2"),
+        FileTag(display_name="Album", id3_key="TALB"),
+        FileTag(display_name="Composer", id3_key="TCOM"),
+        FileTag(display_name="Lyricist", id3_key="TEXT"),
+        FileTag(display_name="Language", id3_key="TLAN"),
+        FileTag(display_name="BPM", id3_key="TBPM"),
+        FileTag(display_name="Key", id3_key="TKEY"),
+        FileTag(display_name="Involved People", id3_key="TIPL"),
+        FileTag(display_name="Musician Credits", id3_key="TMCL"),
     ]
 
     @property
     def file_tags(self) -> list[SongTag]:
         """The tags shown in the main table."""
-        return cast(
-            list[SongTag],
-            self._settings.value("Tags/file_tags", _Settings.default_file_tags),
-        )
+        return self._getArray("Tags/file_tags")
 
     @file_tags.setter
-    def file_tags(self, value: list[SongTag]):
-        self._settings.setValue("Tags/file_tags", value)
+    def file_tags(self, value: list[FileTag]):  # pyright: ignore[reportPropertyTypeMismatch]
+        """Should be a list of FileTag instead of SongTag,
+        so that it's usable in settings.
+        Otherwise you'd need to convert to FileTag, and then back into SongTag later."""
+        self._setArray("Tags/file_tags", value)
 
-    default_standard_tags: list[SongTag] = default_file_tags + [
-        SongTag(display_name="Sort Composer", id3_key="TSOC"),
-        SongTag(display_name="Conductor", id3_key="TPE3"),
-        SongTag(display_name="Year", id3_key="TDRC"),
-        SongTag(display_name="Original Album", id3_key="TOAL"),
-        SongTag(display_name="Original Artist", id3_key="TOPE"),
-        SongTag(display_name="Subtitle", id3_key="TIT3"),
-        SongTag(display_name="Disc Number", id3_key="TPOS"),
-        SongTag(display_name="Set Subtitle", id3_key="TSST"),
-        SongTag(display_name="Sort Artist", id3_key="TSOP"),
-        SongTag(display_name="Sort Album Artist", id3_key="TSO2"),
-        SongTag(display_name="Genre", id3_key="TCON"),
-        SongTag(display_name="Publisher", id3_key="TPUB"),
-        SongTag(display_name="Mood", id3_key="TMOO"),
+    default_standard_tags: list[FileTag] = default_file_tags + [
+        FileTag(display_name="Sort Composer", id3_key="TSOC"),
+        FileTag(display_name="Conductor", id3_key="TPE3"),
+        FileTag(display_name="Year", id3_key="TDRC"),
+        FileTag(display_name="Original Album", id3_key="TOAL"),
+        FileTag(display_name="Original Artist", id3_key="TOPE"),
+        FileTag(display_name="Subtitle", id3_key="TIT3"),
+        FileTag(display_name="Disc Number", id3_key="TPOS"),
+        FileTag(display_name="Set Subtitle", id3_key="TSST"),
+        FileTag(display_name="Sort Artist", id3_key="TSOP"),
+        FileTag(display_name="Sort Album Artist", id3_key="TSO2"),
+        FileTag(display_name="Genre", id3_key="TCON"),
+        FileTag(display_name="Publisher", id3_key="TPUB"),
+        FileTag(display_name="Mood", id3_key="TMOO"),
     ]
 
     @property
     def standard_tags(self) -> list[SongTag]:
         """The tags that are generally defined for music files."""
-        return cast(
-            list[SongTag],
-            self._settings.value("Tags/file_tags", _Settings.default_standard_tags),
-        )
+        return self._getArray("Tags/standard_tags")
 
     @standard_tags.setter
-    def standard_tags(self, value: list[SongTag]):
-        self._settings.setValue("Tags/standard_tags", value)
+    def standard_tags(self, value: list[FileTag]):  # pyright: ignore[reportPropertyTypeMismatch]
+        """Should be a list of FileTag instead of SongTag,
+        so that it's usable in settings.
+        Otherwise you'd need to convert to FileTag, and then back into SongTag later."""
+        self._setArray("Tags/standard_tags", value)
 
-    default_custom_tags: list[SongTag] = [
-        SongTag(display_name="Display Composer", id3_key="TXXX:DISPLAY COMPOSER"),
-        SongTag(display_name="Release Date", id3_key="TXXX:RELEASE DATE"),
-        SongTag(display_name="Original Year", id3_key="TXXX:originalyear"),
-        SongTag(display_name="Scrobble Title", id3_key="TXXX:SCROBBLE TITLE"),
-        SongTag(display_name="Track Name", id3_key="TXXX:TRACK NAME"),
-        SongTag(display_name="Song Type", id3_key="TXXX:SONG TYPE"),
-        SongTag(display_name="Scrobble Album", id3_key="TXXX:SCROBBLE ALBUM"),
-        SongTag(display_name="Album Name", id3_key="TXXX:ALBUM NAME"),
-        SongTag(display_name="Album Type", id3_key="TXXX:ALBUM TYPE"),
-        SongTag(display_name="Album Version", id3_key="TXXX:ALBUM VERSION"),
-        SongTag(display_name="Scrobble Artist", id3_key="TXXX:SCROBBLE ARTIST"),
-        SongTag(display_name="Artist Shown", id3_key="TXXX:ARTIST SHOWN"),
-        SongTag(display_name="Display Artist", id3_key="TXXX:DISPLAY ARTIST"),
-        SongTag(display_name="Label", id3_key="TXXX:LABEL"),
-        SongTag(display_name="Performer", id3_key="TXXX:PERFORMER"),
-        SongTag(display_name="Remixer", id3_key="TXXX:REMIXER"),
-        SongTag(display_name="Occasion", id3_key="TXXX:OCCASION"),
-        SongTag(display_name="Keywords", id3_key="TXXX:KEYWORDS"),
-        SongTag(display_name="Tempo", id3_key="TXXX:TEMPO"),
+    default_custom_tags: list[FileTag] = [
+        FileTag(display_name="Display Composer", id3_key="TXXX:DISPLAY COMPOSER"),
+        FileTag(display_name="Release Date", id3_key="TXXX:RELEASE DATE"),
+        FileTag(display_name="Original Year", id3_key="TXXX:originalyear"),
+        FileTag(display_name="Scrobble Title", id3_key="TXXX:SCROBBLE TITLE"),
+        FileTag(display_name="Track Name", id3_key="TXXX:TRACK NAME"),
+        FileTag(display_name="Song Type", id3_key="TXXX:SONG TYPE"),
+        FileTag(display_name="Scrobble Album", id3_key="TXXX:SCROBBLE ALBUM"),
+        FileTag(display_name="Album Name", id3_key="TXXX:ALBUM NAME"),
+        FileTag(display_name="Album Type", id3_key="TXXX:ALBUM TYPE"),
+        FileTag(display_name="Album Version", id3_key="TXXX:ALBUM VERSION"),
+        FileTag(display_name="Scrobble Artist", id3_key="TXXX:SCROBBLE ARTIST"),
+        FileTag(display_name="Artist Shown", id3_key="TXXX:ARTIST SHOWN"),
+        FileTag(display_name="Display Artist", id3_key="TXXX:DISPLAY ARTIST"),
+        FileTag(display_name="Label", id3_key="TXXX:LABEL"),
+        FileTag(display_name="Performer", id3_key="TXXX:PERFORMER"),
+        FileTag(display_name="Remixer", id3_key="TXXX:REMIXER"),
+        FileTag(display_name="Occasion", id3_key="TXXX:OCCASION"),
+        FileTag(display_name="Keywords", id3_key="TXXX:KEYWORDS"),
+        FileTag(display_name="Tempo", id3_key="TXXX:TEMPO"),
     ]
 
     @property
     def custom_tags(self) -> list[SongTag]:
         """Additional tags which are not common.
         These tags always start with TXXX."""
-        return cast(
-            list[SongTag],
-            self._settings.value("Tags/file_tags", _Settings.default_custom_tags),
-        )
+        return self._getArray("Tags/custom_tags")
 
     @custom_tags.setter
-    def custom_tags(self, value: list[SongTag]):
-        self._settings.setValue("Tags/custom_tags", value)
+    def custom_tags(self, value: list[FileTag]):  # pyright: ignore[reportPropertyTypeMismatch]
+        """Should be a list of FileTag instead of SongTag,
+        so that it's usable in settings.
+        Otherwise you'd need to convert to FileTag, and then back into SongTag later."""
+        self._setArray("Tags/custom_tags", value)
 
     @property
     def all_tags(self) -> list[SongTag]:
         return self.standard_tags + self.custom_tags
+
+    def _setArray(self, key: str, vals: list[FileTag]):
+        self._settings.beginWriteArray(key)
+        for index, tag in enumerate(vals):
+            self._settings.setArrayIndex(index)
+            self._settings.setValue("display_name", tag.display_name)
+            self._settings.setValue("id3_key", tag.id3_key)
+        self._settings.endArray()
+
+    def _getArray(self, key: str) -> list[SongTag]:
+        size = self._settings.beginReadArray(key)
+        tags: list[SongTag] = []
+        for i in range(size):
+            self._settings.setArrayIndex(i)
+            display_name: str = str(self._settings.value("display_name"))
+            id3_key: str = str(self._settings.value("id3_key"))
+            tags.append(SongTag(display_name=display_name, id3_key=id3_key))
+        self._settings.endArray()
+        return tags
 
     def _initializeDefaults(self):
         if not self._settings.contains("Split/split_text_entered"):
