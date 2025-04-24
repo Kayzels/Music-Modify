9eac7327f5b472d12db2c793e3de33ec4aa6000d
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2025-04-24 11:30:59 +0200

    Move main window into its own gui file

--- music_modify/gui/__init__.py
+++ music_modify/gui/__init__.py
@@ -1,5 +1,5 @@
-from .ui_window_main import Ui_MainWindow
 from .dialog_about import AboutDialog
 from .dialog_prefs import PrefsDialog
+from .window_main import MainWindow
 
-__all__ = ["Ui_MainWindow", "AboutDialog", "PrefsDialog"]
+__all__ = ["AboutDialog", "PrefsDialog", "MainWindow"]
