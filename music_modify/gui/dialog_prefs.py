c7aba50bea879af44a0bdd27924244474f3290f3
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2025-04-29 17:04:33 +0200

    [prefs] Let changes to tags actually reflect
    Ensures the settings file is updated, as well as the ui refreshed to
    include those changes.

--- music_modify/gui/dialog_prefs.py
+++ music_modify/gui/dialog_prefs.py
@@ -1,10 +1,11 @@
 import logging
-from typing import TypedDict
+from typing import cast
 
 from PySide6.QtCore import Signal
 from PySide6.QtWidgets import QDialog, QWidget
 
 from music_modify.prefs import prefs
+from music_modify.custom_types import TagInfo
 
 from .dialog_tag import TagDialog
 from .ui_dialog_prefs import Ui_PrefsDialog
@@ -12,18 +13,13 @@ from .ui_dialog_prefs import Ui_PrefsDialog
 logger = logging.getLogger(__name__)
 
 
-class SettingPair(TypedDict):
-    setting_name: str
-    setting_value: str
-
-
 class PrefsDialog(QDialog, Ui_PrefsDialog):
     settings_updated: Signal = Signal()
 
     def __init__(self, /, parent: QWidget | None = None):
         super().__init__(parent)
+        self.changed_settings: dict[str, str | list[TagInfo]] = {}
         self.setupUi(self)  # pyright: ignore[reportUnknownMemberType]
-        self.changed_settings: list[SettingPair] = []
         self.displaySettings()
 
         self.line_edit_split_text_entered.editingFinished.connect(
@@ -41,44 +37,59 @@ class PrefsDialog(QDialog, Ui_PrefsDialog):
                 "split_values_at", self.line_edit_split_values_at.text()
             )
         )
-        self.button_edit_tags.clicked.connect(self.showTags)
+        self.button_edit_tags.clicked.connect(self.editTags)
 
     def displaySettings(self):
         self.line_edit_split_text_entered.setText(prefs.settings.split_text_entered)
         self.line_edit_split_values_display.setText(prefs.settings.split_values_display)
         self.line_edit_split_values_at.setText(prefs.settings.split_values_at)
 
-    def _getSettingChange(self, setting_name: str, setting_value: str):
+    def _getSettingChange(self, setting_name: str, setting_value: str | list[TagInfo]):
         """Gets the value a specific setting has been changed to.
         Stores this in the list of settings to change, which will be reflected
         when the dialog is confirmed."""
-        # TODO: Change format to also deal with lists of tags?
         if setting_value == "":
             # Don't want to use empty string for values
             return
 
-        # Remove the setting for the current name
-        self.changed_settings = [
-            setting
-            for setting in self.changed_settings
-            if setting["setting_name"] != setting_name
-        ]
+        if (
+            isinstance(setting_value, list)
+            or getattr(prefs.settings, setting_name) != setting_value
+        ):
+            self.changed_settings[setting_name] = setting_value
 
-        # Add the new value to the list of changed settings,
-        # if it's different than the original setting.
-        if getattr(prefs.settings, setting_name) != setting_value:
-            self.changed_settings.append(
-                {"setting_name": setting_name, "setting_value": setting_value}
-            )
+    def editTags(self):
+        # Ensure it's a copy, to avoid modifying the existing list.
+        # Check if already in changed_settings, and use that
+        tags: list[TagInfo] = cast(
+            list[TagInfo],
+            self.changed_settings.get("info_tags", prefs.settings.info_tags),
+        ).copy()
 
-    def showTags(self):
-        dialog = TagDialog(self, prefs.settings.info_tags)
+        dialog = TagDialog(self, tags)
+
+        # Other parts of the program shouldn't be interacted with
+        # while the dialog is open
+        dialog.setModal(True)
+
+        def processDialogResult(result: QDialog.DialogCode):
+            # If the dialog is accepted, update the changed_settings dictionary value
+            if result == QDialog.DialogCode.Accepted:
+                self._getSettingChange("info_tags", dialog.model._tags)
+            else:
+                logger.info("Didn't accept tag dialog")
+
+        # TODO: Add warning to TagDialog to say tags are only updated on preference close.
+
+        dialog.finished.connect(processDialogResult)
         dialog.show()
 
     def updateSettings(self):
         if len(self.changed_settings) > 0:
-            for change in self.changed_settings:
-                setattr(prefs.settings, change["setting_name"], change["setting_value"])
+            for setting_name, setting_value in self.changed_settings.items():
+                setattr(prefs.settings, setting_name, setting_value)
             # Create a signal for knowing when it's updated, because this will need
             # to refresh the main table
             self.settings_updated.emit()
+
+    # TODO: Add way of setting back to defaults.
