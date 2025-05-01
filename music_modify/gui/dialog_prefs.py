59d54c371c01f05162e7ebf128400f4fc7470817
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2025-05-01 14:32:27 +0200

    refactor[prefs]: Create single function for processing dialogs
    Rather than duplicating the code for editTags() and editSplit(), create
    a single higher-level function that takes in the dialog type to be
    created, and use this for both.

--- music_modify/gui/dialog_prefs.py
+++ music_modify/gui/dialog_prefs.py
@@ -17,11 +17,13 @@ class PrefsDialog(QDialog, Ui_PrefsDialog):
         super().__init__(parent)
         self.setupUi(self)  # pyright: ignore[reportUnknownMemberType]
 
-        self.button_edit_tags.clicked.connect(self.editTags)
-        self.button_edit_split.clicked.connect(self.editSplit)
+        self.button_edit_tags.clicked.connect(lambda: self.openEditDialog(TagDialog))
+        self.button_edit_split.clicked.connect(
+            lambda: self.openEditDialog(PrefsSplitDialog)
+        )
 
-    def editTags(self) -> None:
-        dialog = TagDialog(self)
+    def openEditDialog(self, dialog_type: type[TagDialog] | type[PrefsSplitDialog]):
+        dialog = dialog_type(self)
         dialog.setModal(True)
 
         # Process accept result
@@ -34,16 +36,7 @@ class PrefsDialog(QDialog, Ui_PrefsDialog):
 
         dialog.show()
 
-    def editSplit(self) -> None:
-        dialog = PrefsSplitDialog(self)
-        dialog.setModal(True)
-
-        # Process accept result
-        def processDialogResult(result: QDialog.DialogCode):
-            if result == QDialog.DialogCode.Accepted:
-                dialog.updateSettings()
-
-        dialog.settings_updated.connect(lambda: self.settings_updated.emit())
-        dialog.finished.connect(processDialogResult)
-
-        dialog.show()
+        # TODO: Create parent class or interface that can be for both.
+        # They both inherit from QDialog, but they have an extra
+        # signal that's needed: settings_updated
+        # Would also be useful to explicitly say what functions are needed.
