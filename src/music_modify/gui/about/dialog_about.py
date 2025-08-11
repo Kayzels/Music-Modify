0e77e803484c3017ffeaa550ea0742a1f0e85f0d
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2025-08-11 16:00:48 +0200

    feat(about): Don't hardcode versions
    Generate the html in Python, rather than having it in the ui file. This
    allows using f-strings, to actually get the versions of the modules when
    running, rather than needing to remember to update them manually.

--- src/music_modify/gui/about/dialog_about.py
+++ src/music_modify/gui/about/dialog_about.py
@@ -1,9 +1,63 @@
-from PySide6.QtWidgets import QDialog, QWidget
+import logging
+import platform
+from datetime import datetime
+
+import mutagen
+import PySide6  # pyright: ignore[reportMissingTypeStubs]
+from PySide6.QtCore import QCoreApplication
+from PySide6.QtWidgets import QApplication, QDialog, QWidget
 
 from .ui_dialog_about import Ui_AboutDialog
+
+logger = logging.getLogger(__name__)
 
 
 class AboutDialog(QDialog, Ui_AboutDialog):
     def __init__(self, parent: QWidget | None = None):
         QDialog.__init__(self, parent)
         self.setupUi(self)
+        text = self.generateText()
+        if text:
+            self.textEdit.setHtml(text)
+
+    @staticmethod
+    def generateText() -> str:
+        instance: QCoreApplication | None = QApplication.instance()
+        if instance is None:
+            logger.warning("Application instance was None when calling generateText")
+            return ""
+        app_name = instance.applicationName()
+        app_version = instance.applicationVersion()
+        app_description = "A music tag editor written using PySide6 and Qt."
+        author = "Kyzan Hartwig"
+        year: int = datetime.now().year
+        python_version: str = platform.python_version()
+        pyside_version: str = PySide6.__version__
+        mutagen_version: str = mutagen.version_string
+
+        return f"""
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "https://www.w3.org/TR/REC-html40/strict.dtd">
+<html>
+    <head>
+        <meta name="qrichtext" content="1"/>
+        <meta charset="utf-8"/>
+        <style>
+            p, h1 {{text-align: center; text-indent: 0;}}
+            h1 {{margin: 0;}}
+        </style>
+    </head>
+    <body>
+        <h1>{app_name}</h1>
+        <p>Version {app_version}</p>
+        <p>{app_description}</p>
+        <p>Copyright © {year} {author}</p>
+        <h2>Tools Used</h2>
+        <ul>
+            <li>Python {python_version}</li>
+            <li>PySide {pyside_version}</li>
+            <li>Qt {pyside_version}</li>
+            <li>mutagen {mutagen_version}</li>
+        </ul>
+    </body>
+</html>
+"""
