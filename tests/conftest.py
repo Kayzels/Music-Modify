d7ab4c74d6ed508740eed6a857865d55005b872d
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2025-08-28 14:10:54 +0200

    tests: Update docs for conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,8 @@
+"""Test configuration and fixtures for pytest.
+
+These fixtures are available in all files in the `tests` folder.
+"""
+
 from collections.abc import Generator
 import os
 from os import PathLike
@@ -12,15 +17,18 @@ import pytest
 from music_modify.prefs.prefs import Settings
 
 NUM_TEMP_SONGS = 3
+"The number of temp songs that should be created."
 
 
 @pytest.fixture
 def num_temp_songs() -> int:
+    """Fixture for the number of temp songs, so tests can access this value."""
     return NUM_TEMP_SONGS
 
 
 @pytest.fixture(scope="session")
 def asset_folder(tmp_path_factory: pytest.TempPathFactory) -> Generator[str]:
+    """The folder where temp songs should be created."""
     temp_dir = tmp_path_factory.mktemp("tmp_songs")
     yield str(temp_dir)
     shutil.rmtree(temp_dir)
@@ -28,6 +36,7 @@ def asset_folder(tmp_path_factory: pytest.TempPathFactory) -> Generator[str]:
 
 @pytest.fixture(scope="session")
 def song_path(tmp_path_factory: pytest.TempPathFactory) -> Generator[Path]:
+    """Path for a single song  for testing adding individual songs."""
     original_file = (Path(__file__).parent / "assets/test_song.mp3").absolute()
     temp_dir = tmp_path_factory.mktemp("tmp_song")
     temp_file = temp_dir / "test_song.mp3"
@@ -39,6 +48,7 @@ def song_path(tmp_path_factory: pytest.TempPathFactory) -> Generator[Path]:
 def song_paths(
     asset_folder: str,
 ) -> list[PathLike[str]]:
+    """Fixture for the temp songs inside the assert folder."""
     original_file = (Path(__file__).parent / "assets/test_song.mp3").absolute()
     files: list[PathLike[str]] = []
     for i in range(1, NUM_TEMP_SONGS + 1):
@@ -50,6 +60,7 @@ def song_paths(
 
 @pytest.fixture
 def temp_settings() -> Generator[Settings]:
+    """Fixture that creates a temporary folder for QSettings."""
     # Create a temp file and keep it until the fixture is done
     fd, path = tempfile.mkstemp()
     os.close(fd)
@@ -64,6 +75,7 @@ def temp_settings() -> Generator[Settings]:
 
 @pytest.fixture(scope="session")
 def app_info(qapp: QApplication) -> tuple[str, str]:
+    """Fixture that creates dummy values for app name and version."""
     name = "Test Music Modify"
     version = "9.9.9"
     qapp.setApplicationName(name)
