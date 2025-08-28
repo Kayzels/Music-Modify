8c98d4fc8773372993e3cc05025952f6493b38a8
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2025-08-28 14:15:25 +0200

    tests: Update prefs tests
    Mainly to ensure it follows the structure of
    test_ClassName_functionName_details.

--- tests/prefs/test_prefs.py
+++ tests/prefs/test_prefs.py
@@ -2,20 +2,21 @@ from music_modify.custom_types import SongTag, TagInfo
 from music_modify.prefs.prefs import Settings
 
 
-def set_split_values(settings: Settings) -> None:
+def _set_split_values(settings: Settings) -> None:
     settings.split_text_entered = "-"
     settings.split_values_at = "-"
     settings.split_values_display = "-"
 
 
-def set_tag_values(settings: Settings) -> None:
+def _set_tag_values(settings: Settings) -> None:
     settings.info_tags = [
         TagInfo(id3_key="TIT2", display_name="Title", show_in_table=True),
         TagInfo(id3_key="TPE2", display_name="Artist", show_in_table=False),
     ]
 
 
-def test_init(temp_settings: Settings) -> None:
+def test_Settings_init(temp_settings: Settings) -> None:
+    """Test that a Settings object is created correctly."""
     # Test _initializeDefaults
     assert temp_settings.split_text_entered == Settings.default_split_text_entered
     assert temp_settings.split_values_display == Settings.default_split_values_display
@@ -36,15 +37,17 @@ def test_init(temp_settings: Settings) -> None:
     ]
 
 
-def test_set_split(temp_settings: Settings) -> None:
-    set_split_values(temp_settings)
+def test_Settings_set_split(temp_settings: Settings) -> None:
+    """Test that setting the properties for split values works."""
+    _set_split_values(temp_settings)
     assert temp_settings.split_text_entered == "-"
     assert temp_settings.split_values_at == "-"
     assert temp_settings.split_values_display == "-"
 
 
-def test_set_tag(temp_settings: Settings) -> None:
-    set_tag_values(temp_settings)
+def test_Settings_set_tag(temp_settings: Settings) -> None:
+    """Test that settings the properties for tags works."""
+    _set_tag_values(temp_settings)
     assert temp_settings.info_tags == [
         TagInfo(id3_key="TIT2", display_name="Title", show_in_table=True),
         TagInfo(id3_key="TPE2", display_name="Artist", show_in_table=False),
@@ -52,8 +55,9 @@ def test_set_tag(temp_settings: Settings) -> None:
     assert temp_settings.table_tags == [SongTag(id3_key="TIT2", display_name="Title")]
 
 
-def test_resetSplit(temp_settings: Settings) -> None:
-    set_split_values(temp_settings)
+def test_Settings_resetSplit(temp_settings: Settings) -> None:
+    """Test that resetting split values changes back to the original default ones."""
+    _set_split_values(temp_settings)
     temp_settings.resetSplit()
     assert temp_settings.split_text_entered == Settings.default_split_text_entered
     assert temp_settings.split_values_at == Settings.default_split_values_at
@@ -61,7 +65,8 @@ def test_resetSplit(temp_settings: Settings) -> None:
 
 
 def test_resetTags(temp_settings: Settings) -> None:
-    set_tag_values(temp_settings)
+    """Test that resetting tags changes back to the original default ones."""
+    _set_tag_values(temp_settings)
     temp_settings.resetTags()
 
     # Test all_tags
